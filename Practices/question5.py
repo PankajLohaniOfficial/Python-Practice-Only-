4e1f73b1f96478aa71386775543e469bfba53733
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-19 15:51:30 +0530

    Create question5.py

---
+++ Practices/question5.py
@@ -0,0 +1,6 @@
+# Write a Program to find the average of the two numbers entered by the user :
+a = int ( input ( " Enter the First Number : "))
+b = int ( input ( " Enter the Second Number : "))
+
+average = (a+b)/2
+print ( "The average of the Two numbers entered is " , average )
