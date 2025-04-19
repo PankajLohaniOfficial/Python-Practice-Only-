d4d557307a02ef0844c8da1e1137c00b7485aebe
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-19 15:26:22 +0530

    Create question2.py

---
+++ Practices/question2.py
@@ -0,0 +1,5 @@
+#WAP to add the two numbers by taking them input from the user:
+a = int ( input ( " Enter the Number1 : " ))
+b = int ( input ( " Enter the Number2 : " ))
+sum = a + b
+print ( " The Sum of the two number is : " , sum )
