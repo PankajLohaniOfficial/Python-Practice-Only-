5d9ed94ffe005ecca197af8ae308198b1adb1c6d
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-19 15:30:40 +0530

    Create ques3.py

---
+++ Practices/ques3.py
@@ -0,0 +1,5 @@
+#Wap to find the remainder when the number is divided by the z:
+a = int ( input ( " Enter the Number to be divided : "))
+b = int ( input ( " Enter the Number from which we have to divide : " ))
+div = a/b
+print ( div )
