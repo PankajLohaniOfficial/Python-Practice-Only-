9646bba5a7629d2aca4c26a26c58877479863727
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-19 15:48:01 +0530

    Create question4.py

---
+++ Practices/question4.py
@@ -0,0 +1,3 @@
+# Take the input from the user and check the type of the variable entered by the user
+a = input ( " Enter the value of the variable a :- " )
+print ( "The type of a is " , type (a) )
