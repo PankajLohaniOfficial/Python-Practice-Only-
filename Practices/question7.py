a832d0059577a06ecdbb10f3bdd26e063d2ce739
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-20 19:07:53 +0530

    Create question7.py

---
+++ Practices/question7.py
@@ -0,0 +1,3 @@
+# Write a Python program to display a user entered name and followed by the good afternoon using input () function
+name = input ( " Enter your Name :- " )
+print ( name , "Good Afternoon" )
