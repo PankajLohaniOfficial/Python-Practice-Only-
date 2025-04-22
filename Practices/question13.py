9ab7818a6aaf809016f00eca75b203f598d5f2cf
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-22 17:10:45 +0530

    Create question13.py

---
+++ Practices/question13.py
@@ -0,0 +1,11 @@
+# Can we have the set with 18 (int) and ' 18 ' (string) as a value in it:
+
+s = set()
+s.add(18)
+s.add("18")
+
+print (s)
+
+
+#Output:-
+#Yes we can have the sets with 18 as a integer and can also have 18 as an string
