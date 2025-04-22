f0dd66aaef430a95ad16d08062da09f101d66822
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-22 15:43:49 +0530

    Create question12.py

---
+++ Practices/question12.py
@@ -0,0 +1,27 @@
+# Write a Program to input the eight numbers from the user and display all the unique numbers(once)
+s = set()
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+n = int ( input ( " Enter the Number 1" ))
+s.add(int(n))
+
+print(s)
