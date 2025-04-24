9997071627e89d5ea90b47566fab090d7ba1ea54
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-24 16:48:19 +0530

    Create question15.py

---
+++ Practices/question15.py
@@ -0,0 +1,29 @@
+#Create an empty dictionary . allow 4 friends to enter their favourite language as the value and ue key as their names . assume that the names are unique.
+
+d = {}
+name = input ( "Enter Friends Name :- " )
+lang = input ( "Enter the Language name :- " )
+
+d.update({name: lang})
+
+
+name = input ( "Enter Friends Name :- " )
+lang = input ( "Enter the Language name :- " )
+
+d.update({name: lang})
+
+
+name = input ( "Enter Friends Name :- " )
+lang = input ( "Enter the Language name :- " )
+
+d.update({name: lang})
+
+
+name = input ( "Enter Friends Name :- " )
+lang = input ( "Enter the Language name :- " )
+
+d.update({name: lang})
+
+
+
+print (d)
