5efa584ba3e3022fd35daf5b73939eb04a0b1fed
PBrawler <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-04-27 16:51:16 +0530

    Create question19.py

---
+++ Practices/question19.py
@@ -0,0 +1,8 @@
+# Write a program to find that whether the given username contains less than 10 characters or not:-
+
+username = input ( "Enter the username :- " )
+
+if (len(username)<10):
+  print ("Your username contains less than the 10 characters " )
+else:
+  print ("Your username contains more than or equals to 10 characters")
